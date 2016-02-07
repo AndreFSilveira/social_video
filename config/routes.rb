a726609929eca6abcbbc392b3920d4132a16da16
André Felipe Silveira <andrefelipesilveira@yahoo.com.br>
2016-02-06 22:38:26 -0200

    create login area witch remember token

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create', as: 'login_path'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :tags
   resources :videos
   resources :news
-  get 'users/new'
+  resources :users
 
 end
